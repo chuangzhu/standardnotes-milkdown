3993dd43126d52d87b497ecbacd6e3a2540df341
Chuang Zhu <chuang@melty.land>
2021-09-18 23:55:29 +0800

    load text, render it, edit and save

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,54 @@
-import './style.css'
+import "./style.css";
+import { EditorKit, EditorKitDelegate } from "sn-editor-kit";
+import { defaultValueCtx, Editor, rootCtx } from "@milkdown/core";
+import { gfm } from "@milkdown/preset-gfm";
+import { nord } from "@milkdown/theme-nord";
+import { listener, listenerCtx } from '@milkdown/plugin-listener';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+class MilkdownEditor {
+  editorKit: any;
+  constructor() {
+    // Initialize an empty editor anyway (for browsers)
+    this.initMilkdown("");
+    const delegate = new EditorKitDelegate({
+      setEditorRawText: (text: string) => {
+        // Re-render the editor once SN give us text
+        this.initMilkdown(text);
+      },
+    });
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+    this.editorKit = new EditorKit({
+      delegate: delegate,
+      mode: "plaintext",
+      supportsFilesafe: false,
+    });
+  }
+
+  initMilkdown(defaultValue: string) {
+    Editor.make()
+      .config((ctx) => {
+        ctx.set(rootCtx, document.getElementById("app"));
+        ctx.set(defaultValueCtx, defaultValue);
+          ctx.set(listenerCtx, {
+            markdown: [(get) => this.saveNote(get())],
+          });
+      })
+      .use(nord)
+      .use(gfm)
+      .use(listener)
+      .create();
+  }
+
+  saveNote(text: string) {
+    /** This will work in an SN context, but breaks the standalone editor,
+     * so we need to catch the error
+     */
+    try {
+      this.editorKit.onEditorValueChanged(text);
+    } catch (error) {
+      console.log("Error saving note:", error);
+    }
+  }
+}
+
+new MilkdownEditor();
